fab28ca2bc6a80f180fb79e8b8dbfc7821d9e583
lrlunin <lunin@campus.tu-berlin.de>
2022-02-02 22:49:30 +0100

    implemented StateOne, dummy StopOne and AbortOne

--- MOENCHTangoTwoDController.py
+++ MOENCHTangoTwoDController.py
@@ -1,8 +1,10 @@
 from sardana.pool.controller import TwoDController, Referable
-from tango import Device, DeviceProxy, DevFailed
+from tango import DeviceProxy, DevState
+import numpy as np
 
 
 class MOENCHTangoTwoDController(TwoDController, Referable):
+    "This class is the Tango Sardana Two D controller for the MOENCH PSI"
     ctrl_properties = {
         "controlMOENCHTangoFQDN": {
             Type: str,
@@ -13,20 +15,6 @@ class MOENCHTangoTwoDController(TwoDController, Referable):
             Type: str,
             Description: "The FQDN of the PI-MTE tango DS",
             DefaultValue: "rsxs/moenchAcquire/bchip286",
-        },
-        "PointNb": {
-            Type: int,
-            Description: "pointNb of the scan",
-            Access: DataAccess.ReadWrite,
-            FGet: "getPointNb",
-            FSet: "setPointNb",
-        },
-        "FileName": {
-            Type: str,
-            Description: "file name of image",
-            Access: DataAccess.ReadWrite,
-            FGet: "getFileName",
-            FSet: "setFileName",
         },
     }
     axis_attributes = {
@@ -63,29 +51,34 @@ class MOENCHTangoTwoDController(TwoDController, Referable):
 
     def ReadOne(self, axis):
         """Get the specified counter value"""
-        # print(self._SavingEnabled)
-        # print('Image saved to: {:s}'.format(self.proxy.LastSavedImage))
-        return self.proxy.image
+        # TODO: unclear what need to return in case of reference
+        return np.zeros(400, 400)
 
     def RefOne(self, axis):
-        return self.proxy.LastSavedImage
+        return self.control_device.tiff_fullpath_last
 
     def SetAxisPar(self, axis, parameter, value):
-        #        if parameter == "value_ref_pattern":
-        #            print('value_ref_pattern ' + str(value))
-        #        elif parameter == "value_ref_enabled":
-        #            print('value_ref_enabled ' + str(value))
-        #            self.setSavingEnabled(axis, value)
         pass
 
     def StateOne(self, axis):
         """Get the specified counter state"""
-        #
-        return self.control_device.detector_status, "Counter is acquiring or not"
+        acquire_state = self.acquire_device.state
+        if acquire_state == DevState.ON:
+            tup = (acquire_state, "Camera ready")
+        elif acquire_state == DevState.FAULT:
+            tup = (acquire_state, "Camera in FAULT state")
+        elif acquire_state == DevState.STANDBY:
+            acquire_state = DevState.MOVING
+            tup = (acquire_state, "Camera is waiting for trigger")
+        elif acquire_state == DevState.RUNNING:
+            # according to https://www.sardana-controls.org/devel/howto_controllers/howto_0dcontroller.html?highlight=stateone need to be set to MOVING while acquiring
+            acquire_state = DevState.MOVING
+            tup = (acquire_state, "Camera acquiring")
+        return acquire_state, "Counter is acquiring or not"
 
     def PrepareOne(self, axis, value, repetitions, latency, nb_starts):
         # set exporsure time of GE cam
-        self.proxy.ExposureTime = float(value)
+        self.control_device.exposure = float(value)
 
     def LoadOne(self, axis, value, repetitions, latency):
         pass
@@ -97,15 +90,16 @@ class MOENCHTangoTwoDController(TwoDController, Referable):
 
     def StopOne(self, axis):
         """Stop the specified counter"""
-        self.control_device.rx_stop()
-        # self.proxy.StopAcq()
+        # FIXME: pending https://github.com/MBI-Div-B/pytango-moenchDetector/issues/13
+        pass
 
     def AbortOne(self, axis):
         """Abort the specified counter"""
-        self.control_device.rx_stop()
-        # self.proxy.StopAcq()
+        # FIXME: pending https://github.com/MBI-Div-B/pytango-moenchDetector/issues/13
+        pass
 
     def isSavingEnabled(self, axis):
+        # TODO: check if a zmqfreq turn off is required
         return self.control_device.filewrite
 
     def setSavingEnabled(self, axis, value):
